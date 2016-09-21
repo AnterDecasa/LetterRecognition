17e9421bd0d7426d8dcd68457a04379c24f55e8b
AnterDecasa <anterdecasa@gmail.com>
2016-09-21 14:17:13 +0800

    first commit

---
+++ src/letterrecognition/LetterRecognition.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package letterrecognition;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+/**
+ *
+ * @author anter_000
+ */
+public class LetterRecognition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+         * Test Code
+         */
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
+        
+        /**
+        * Camera Access
+        */
+        
+    }
+    
+}
