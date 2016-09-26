9396af4ffaeb217f1a345cc33aea09299fef6f30
AnterDecasa <anterdecasa@gmail.com>
2016-09-26 15:15:56 +0800

    Final UI

--- src/letterrecognition/LetterRecognition.java
+++ src/letterrecognition/LetterRecognition.java
@@ -5,9 +5,22 @@
  */
 package letterrecognition;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+//import org.opencv.core.Core;
+//import org.opencv.core.CvType;
+//import org.opencv.core.Mat;
 
 /**
  *
@@ -18,18 +31,58 @@ public class LetterRecognition {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        /**
-         * Test Code
-         */
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat = " + mat.dump());
+    public static void main(String[] args) throws InterruptedException {
         
-        /**
-        * Camera Access
-        */
+        JFrame window = new JFrame("Test webcam panel");
         
+        JPanel mainPanel = new JPanel();
+        JPanel controlPanel = new JPanel();
+        
+        //Access the webCam
+        Webcam webcam = Webcam.getDefault();
+        webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+        WebcamPanel liveFeedPanel = new WebcamPanel(webcam);
+        liveFeedPanel.setFPSDisplayed(true);
+        liveFeedPanel.setDisplayDebugInfo(true);
+        liveFeedPanel.setImageSizeDisplayed(true);
+        liveFeedPanel.setMirrored(true);
+        
+        //Create the Control Panel
+        JButton captureBtn = new JButton("Capture");
+        captureBtn.setSize(40, 20);
+        captureBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // get image
+		BufferedImage image = webcam.getImage();
+
+		// save image to PNG file
+                try{
+                    ImageIO.write(image, "PNG", new File("test.png"));
+                }
+                catch(IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+            
+        });
+        
+        //Add all elements of Control Panel
+        controlPanel.add(captureBtn);
+        
+        //Add all sub panels to Main Panel
+        mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
+        mainPanel.add(liveFeedPanel);
+        mainPanel.add(controlPanel);
+        mainPanel.setVisible(true);
+        
+        //Set Window
+        window.add(mainPanel);
+        window.setResizable(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
     }
     
 }
